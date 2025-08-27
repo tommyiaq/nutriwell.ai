9e0fa11bbd5bad184ccffbfdbec549d5a39beecc
tommyiaqmenumal <tommaso.iaquinta@menumal.it>
2025-08-27 11:00:32 +0200

    exported static

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
-module.exports = {
-  output: 'export',
-};
-
-// To export on github uncomment this and run npm run build, npm run export
 // module.exports = {
-//   basePath: '/nutrivell.ai',
-//   assetPrefix: '/nutrivell.ai/',
 //   output: 'export',
 // };
+
+// To export on github uncomment this and run npm run build, npm run export
+module.exports = {
+  basePath: '/nutrivell.ai',
+  assetPrefix: '/nutrivell.ai/',
+  output: 'export',
+};
