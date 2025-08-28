ad27dfb84ea677f4cf3454e6f386dcfed07bf4c5
tommyiaqmenumal <tommaso.iaquinta@menumal.it>
2025-08-28 14:27:50 +0200

    better rendering in mobile

---
+++ src/pages/_document.tsx
@@ -0,0 +1,17 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+        <meta name="description" content="nutriwell - The First AI-Powered Digital Nutritionist" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
